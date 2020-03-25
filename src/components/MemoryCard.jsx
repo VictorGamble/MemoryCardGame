82531eb54f9ced2e2ec939e857eb8067802d5462
Victor <Victor.Gamble@outlook.com>
2020-03-25 10:03:02 -0400

    added styling for the following classes MemCardback, MemCardInner, MemCard, MemCardFront and some chained classes

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-import './MemoryCard.css';
+import React, { Component } from "react";
+import "./MemoryCard.css";
 
-class MemoryCard extends Component{
-    render() {
-        return (
-            <div className="MemoryCard">
-                <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="DC Logo"/>
-            </div>
-        )
-    }
+class MemoryCard extends Component {
+  render() {
+    return (
+      <div className="MemoryCard">
+        <div className="MemomryCardInner">
+          <div className="MemoryCardBack">
+            <img
+              src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"
+              alt="DC Logo"
+            />
+          </div>
+          <div className="MemoryCardFront">
+            <p>∆</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default MemoryCard;
