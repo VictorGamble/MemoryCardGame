ced826a3dd530a6150251e69691703f4e8136b15
Victor <Victor.Gamble@outlook.com>
2020-03-26 11:25:23 -0400

    refactored app.js, starting to create the symbols for the card now

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import MemoryCard from './components/MemoryCard'
 import './App.css';
 import { render } from '@testing-library/react';
 
+
+
+
 class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = { isFlipped: false };
+  state = {
+    deck: [],
+    pickedCards: []
   }
   clickHandler = () => {
     this.setState({ isFlipped: true });
   };
   render() {
-    let memoryCardInnerClass = "MemoryCardInner";
-    let { isFlipped } = this.state;
-    isFlipped === false
-      ? (memoryCardInnerClass = "MemoryCardBack")
-      : (memoryCardInnerClass = "MemoryCardInner flipped");
+    
     return (
       <div className="App">
         <header className="App-header">
