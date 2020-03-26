1dc2d1d11d8dc125eea80c6f5a7426b002d30bda
Victor <Victor.Gamble@outlook.com>
2020-03-26 11:02:50 -0400

    refactored the stateless function App() to a class component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import MemoryCard from './components/MemoryCard'
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2 className="title">Memory Game</h2>
-        <h3 className="subtitle">Match Cards to win</h3>
-      </header>
-      <div className='row'>
-        < MemoryCard />
-        <MemoryCard />
-        < MemoryCard />
-        <MemoryCard/>
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { isFlipped: false };
+  }
+  clickHandler = () => {
+    this.setState({ isFlipped: true });
+  };
+  render() {
+    let memoryCardInnerClass = "MemoryCardInner";
+    let { isFlipped } = this.state;
+    isFlipped === false
+      ? (memoryCardInnerClass = "MemoryCardBack")
+      : (memoryCardInnerClass = "MemoryCardInner flipped");
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2 className="title">Memory Game</h2>
+          <h3 className="subtitle">Match Cards to win</h3>
+        </header>
+        <div className='row'>
+          < MemoryCard />
+          <MemoryCard />
+          < MemoryCard />
+          <MemoryCard />
+        </div>
+        <div className='row'>
+          < MemoryCard />
+          < MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div className='row'>
+          < MemoryCard />
+          < MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div className='row'>
+          < MemoryCard />
+          < MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
       </div>
-      <div className='row'>
-        < MemoryCard />
-        < MemoryCard />
-        <MemoryCard />
-        <MemoryCard/>
-      </div>
-      <div className='row'>
-        < MemoryCard />
-        < MemoryCard />
-        <MemoryCard />
-        <MemoryCard/>
-      </div>
-      <div className='row'>
-        < MemoryCard />
-        < MemoryCard />
-        <MemoryCard />
-        <MemoryCard/>
-       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
